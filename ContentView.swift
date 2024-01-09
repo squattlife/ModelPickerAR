e967869cc03569d055212ca4de0ff81d6f6e0e66
squattlife <migonyoung01@gmail.com>
2024-01-09 17:52:06 +0900

    Create Models PickerUI

--- ContentView.swift
+++ ContentView.swift
@@ -9,10 +9,17 @@ import SwiftUI
 import RealityKit
 
 struct ContentView : View {
+    var models: [String] = ["fender_stratocaster",
+                            "sneaker_airforce", "teapot", "toy_biplane_idle"]
+    
     var body: some View {
         ARViewContainer().edgesIgnoringSafeArea(.all)
         
-        Text("test Label")
+        ZStack(alignment: .bottom) {
+            ARViewContainer()
+            
+            ModelPickerView(models: self.models)
+        }
     }
 }
 
@@ -28,6 +35,32 @@ struct ARViewContainer: UIViewRepresentable {
     
     func updateUIView(_ uiView: ARView, context: Context) {}
     
+}
+
+struct ModelPickerView: View {
+    var models: [String]
+    
+    var body: some View {
+        ScrollView(.horizontal, showsIndicators: false) {
+            HStack(spacing: 30) {
+                ForEach(0 ..< self.models.count) { index in
+                    Button(action: {
+                        print("DEBUG: selected model with name: \(self.models[index])")
+                    }) {
+                        Image(uiImage: UIImage(named: self.models[index])!)
+                            .resizable()
+                            .frame(height: 80)
+                            .aspectRatio(1/1, contentMode: .fit)
+                            .cornerRadius(15)
+                        
+                    }
+                    .buttonStyle(PlainButtonStyle())
+                }
+            }
+        }
+        .padding(20)
+        .background(.black.opacity(0.5))
+    }
 }
 
 #if DEBUG
